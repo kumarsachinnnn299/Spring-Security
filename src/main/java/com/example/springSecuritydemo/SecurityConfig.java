cb8f95a60aa836207674ed25ef8cd79422ff2f82
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-09-19 23:23:33 +0530

    Implemented in-memory user authentication

---
+++ src/main/java/com/example/springSecuritydemo/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.springSecuritydemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+
+//        to make authentication stateless
+        http.sessionManagement(session->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+//        http.formLogin(Customizer.withDefaults());// this will enable form based authentication
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.withUsername("user1")
+                .password("{noop}Sachin@123")
+                .roles("USER")
+                .build();
+        UserDetails admin= User.withUsername("admin")
+                .password("{noop}adminPass")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,admin);
+    }
+
+}
