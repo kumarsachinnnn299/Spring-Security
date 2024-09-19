104ece30549506c58415b2cde0b8122be5525150
Sachin Kumar Singh <kumarsachinnnn299@gmail.com>
2024-09-19 23:36:12 +0530

    Implemented role based authorization

--- src/main/java/com/example/springSecuritydemo/GreetingsController.java
+++ src/main/java/com/example/springSecuritydemo/GreetingsController.java
@@ -1,12 +1,25 @@
 package com.example.springSecuritydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingsController {
-@GetMapping("/hello")
+
+@GetMapping("/hello")//This endpoint is accssible to all users
     public String sayHello(){
         return "Hello";
     }
+    @GetMapping("/user")//This endpoint is accssible to normal user only
+    @PreAuthorize("hasRole('USER')")
+    public String userEndpoint(){
+        return "Hello User!!";
+    }
+    @GetMapping("/admin")//This endpoint is accssible to admin users only
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminEndpoint(){
+        return "Hello Admin!!!";
+    }
+
 }
